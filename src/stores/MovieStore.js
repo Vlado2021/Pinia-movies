5dd62464ae6b45ca06438305270ed9f5ee2def78
Vlado2021 <92333488+Vlado2021@users.noreply.github.com>
2024-04-04 11:54:15 +0300

    20240404 11:54 worked with actions

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -31,5 +31,18 @@ export const useMovieStore = defineStore("movieStore", {
     totalCountMovies(){
       return this.movies.length;
     },
+  },
+  actions: {
+    setActiveTab(id){
+      this.activeTab=id;
+
+    },
+    toggleWatched(id){
+      const idx=this.movies.findIndex(el=>el.id===id);
+      this.movies[idx].isWatched=!this.movies[idx].isWatched;
+    },
+    deleteMovies(id) {
+      this.movies=this.movies.filter(el=>el.id!==id);
+    }
   }
 });
